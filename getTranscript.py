0d9b3d5fa923a678c78a65675b5d62aa5cdf1982
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-05-28 02:08:55 +0530

    Update getTranscript.py

--- getTranscript.py
+++ getTranscript.py
@@ -1,11 +1,18 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-srt = YouTubeTranscriptApi.get_transcript("M576WGiDBdQ", languages=['en'])
-# print(len(srt))
-with open("subtitles.txt", "w+") as f:
-    for i in range(len(srt)):
-        f.write("{}\n".format(srt[i]['text']))
-        if i == len(srt) - 1:
-            print('Successfully Created Subtitles File')
+url = input("Enter the URL of the youtube video: ").split('/')
+# print(url[-1][8:])
+try:
+    srt = YouTubeTranscriptApi.get_transcript(url[-1][8:], languages=['en'])
+    # print(len(srt))
+    with open("subtitles.txt", "w+") as f:
+        for i in range(len(srt)):
+            f.write("{}\n".format(srt[i]['text']))
+            if i == len(srt) - 1:
+                print('Successfully Created Subtitles File')
+    f.close()
 
-f.close()
+except Exception as e:
+    print('-'*90)
+    print("An Exception Occurred : ")
+    print(e)
